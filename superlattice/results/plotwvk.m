eec1989dca35d5f520a7fcedde37a4617430bc24
Samuel Huberman <samuel.huberman@gmail.com>
2012-07-31 13:43:58 -0400

    better plot function

--- superlattice/results/plotwvk.m
+++ superlattice/results/plotwvk.m
@@ -6,56 +6,26 @@ NMD.x0.LJ.mass = 6.6326E-26;
 NMD.x0.LJ.tau = sqrt((NMD.x0.LJ.mass*(NMD.x0.LJ.sigma)^2)/NMD.x0.LJ.eps);
 kb = 1.3806E-23; 
 
-p4_freq='4p_freq.dat'
-p8_freq='8p_freq.dat'
-p12_freq='12p_freq.dat'
-p16_freq='16p_freq.dat'
+period=[4,6,8,10,12,16]
+colour=['b','c','g','m','r','k']
 
-p4_vel='4p_vel.dat'
-p8_vel='8p_vel.dat'
-p12_vel='12p_vel.dat'
-p16_vel='16p_vel.dat'
+for i=1:1:length(period)
+    str_freq=strcat(num2str(period(i)),'p_freq.dat');
+    str_vel=strcat(num2str(period(i)),'p_vel.dat');
+    str_x0=strcat(num2str(period(i)),'p_x0.dat');
+    str_life=strcat(num2str(period(i)),'p_life.dat');
+    [f,kw]=omegabin(str_freq,str_vel,str_life,str_x0);
+   
+    semilogx(f,kw,colour(i));
+    hold on
+    yL = get(gca,'YLim');
+    line([period(i)*1*0.78161*NMD.x0.LJ.sigma period(i)*1*0.78161*NMD.x0.LJ.sigma],yL,'Color',colour(i));
+    hold on
+end
 
-p4_life='4p_life.dat'
-p8_life='8p_life.dat'
-p12_life='12p_life.dat'
-p16_life='16p_life.dat'
-
-p4_x0='4p_x0.dat'
-p8_x0='8p_x0.dat'
-p12_x0='12p_x0.dat'
-p16_x0='16p_x0.dat'
-
-p6_freq='6p_freq.dat'
-p6_vel='6p_vel.dat'
-p6_life='6p_life.dat'
-p6_x0='6p_x0.dat'
-
-p10_freq='10p_freq.dat'
-p10_vel='10p_vel.dat'
-p10_life='10p_life.dat'
-p10_x0='10p_x0.dat'
-
-[p4f,p4_kw] =omegabin(p4_freq,p4_vel,p4_life,p4_x0);
-[p6f,p6_kw] =omegabin(p6_freq,p6_vel,p6_life,p6_x0);
-[p8f,p8_kw] =omegabin(p8_freq,p8_vel,p8_life,p8_x0);
-[p10f,p10_kw] =omegabin(p10_freq,p10_vel,p10_life,p10_x0);
-[p12f,p12_kw] =omegabin(p12_freq,p12_vel,p12_life,p12_x0);
-[p16f,p16_kw] =omegabin(p16_freq,p16_vel,p16_life,p16_x0);
-
-semilogx(p4f,p4_kw,'bo',p6f,p6_kw,'co',p8f,p8_kw,'go',p10f,p10_kw,'mo',p12f,p12_kw,'ro',p16f,p16_kw,'ko')
-legend('4p','6p','8p','10p','12p','16p');
 xlabel('Mean Free Path [m]')
 ylabel('Thermal Conductivity [W/m K]')
-
-yL = get(gca,'YLim');
-hold on
-line([4*2*0.78161*NMD.x0.LJ.sigma 4*2*0.78161*NMD.x0.LJ.sigma],yL,'Color','b');
-line([6*2*0.78161*NMD.x0.LJ.sigma 6*2*0.78161*NMD.x0.LJ.sigma],yL,'Color','c')
-line([8*2*0.78161*NMD.x0.LJ.sigma 8*2*0.78161*NMD.x0.LJ.sigma],yL,'Color','g')
-line([10*2*0.78161*NMD.x0.LJ.sigma 10*2*0.78161*NMD.x0.LJ.sigma],yL,'Color','m')
-line([12*2*0.78161*NMD.x0.LJ.sigma 12*2*0.78161*NMD.x0.LJ.sigma],yL,'Color','r')
-line([16*2*0.78161*NMD.x0.LJ.sigma 16*2*0.78161*NMD.x0.LJ.sigma],yL,'Color','k')
+line([2.5*NMD.x0.LJ.sigma 2.5*NMD.x0.LJ.sigma],yL,'Color','y')
 
 end
 
